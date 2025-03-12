34ce8a19489fc6bac8dcf63d6b6e8d9c77742c74
imaun <imun22@gmail.com>
2025-03-12 23:21:57 +0100

    add ClaudeTranslator class

--- src/translator.ts
+++ src/translator.ts
@@ -4,12 +4,14 @@ class Translator {
     }
 }
 
+const LANGSHIFT_API_KEY: string = 'langshift_api_key';
+
 class OpenAITranslator extends Translator {
 
     async translate(text: string, targetLang: string, model: string) {
         const _apiUrl: string = 'https://api.openai.com/v1/chat/completions';
 
-        const { openai_api_key: apiKey } = await chrome.storage.sync.get(['openai_api_key']);
+        const { openai_api_key: apiKey } = await chrome.storage.sync.get([LANGSHIFT_API_KEY]);
         if(!apiKey) {
             console.warn('⚠️ No OpenAI API key found.')
             return null;
@@ -35,6 +37,43 @@ class OpenAITranslator extends Translator {
             return data.choices?.[0]?.message?.content?.trim() || "⚠️ Translation failed.";
         } catch(error) {
             console.error("OpenAI Translation failed:", error);
+            return null;
+        }
+    }
+}
+
+class ClaudeTranslator extends Translator {
+
+    async translate(text: string, targetLang: string, model: string) {
+        const _apiUrl = 'https://api.anthropic.com/v1/messages';
+
+        const { claude_api_key: apiKey } = await chrome.storage.sync.get([LANGSHIFT_API_KEY]);
+        if(!apiKey) {
+            console.warn('⚠️ No Calude API key found.');
+            return null;
+        }
+
+        const prompt = `Translate the following text to ${targetLang}: \"${text}\"`;
+
+        try {
+            const response = await fetch(_apiUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-api-key': apiKey,
+                    'anthropic-version': '2023-06-01'
+                },
+                body: JSON.stringify({
+                    model: model || "claude-2",
+                    messages: [{ role: "user", content: prompt }],
+                }),
+            });
+
+            const data = await response.json();
+
+            return data.content?.[0]?.text?.trim() || "⚠️ Translation failed.";
+        } catch(error) {
+            console.error("Claude Translation failed:", error);
             return null;
         }
     }
