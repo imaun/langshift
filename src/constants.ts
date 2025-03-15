cf569a87539ed1f7549b823d69154bcc5752e632
imaun <imun22@gmail.com>
2025-03-15 17:17:58 +0100

    remove Azure

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,4 @@ export class Constants {
     static readonly OPENAI_API_KEY: string = 'openai_api_key';
     static readonly CLAUDE_API_KEY: string = 'claude_api_key';
     static readonly GEMINI_API_KEY: string = 'gemini_api_key';
-    static readonly AZURE_API_KEY: string = 'azure_api_key';
 }
